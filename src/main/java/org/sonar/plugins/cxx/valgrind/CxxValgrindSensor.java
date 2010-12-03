0001daf75a26fdce6403b55ed7db7e5ad8559052
Vincent Hardion <vincent.hardion@gmail.com>
2010-12-03 16:53:55 +0000

    SONARPLUGINS-848 : format license header by mvn license:format

--- src/main/java/org/sonar/plugins/cxx/valgrind/CxxValgrindSensor.java
+++ src/main/java/org/sonar/plugins/cxx/valgrind/CxxValgrindSensor.java
@@ -1,20 +1,19 @@
 /*
- * Sonar Cxx Plugin, open source software quality management tool.
- * Copyright (C) 2010 Franck Bonin
- * mailto:contact AT sonarsource DOT com
+ * SonarCxxPlugin, open source software for C++ quality management tool.
+ * Copyright (C) 2010 François DORIN, Franck Bonin
  *
- * Sonar is free software; you can redistribute it and/or
+ * SonarCxxPlugin is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * Sonar is distributed in the hope that it will be useful,
+ * SonarCxxPlugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Sonar; if not, write to the Free Software
+ * License along with SonarCxxPlugin; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
 
