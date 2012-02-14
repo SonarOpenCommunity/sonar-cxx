9ee2b63578649209f691ef43b288d7a501df4978
Waleri Enns <waleri.enns@gmail.com>
2012-02-14 09:16:53 +0000

    Replace the accented characters in the license header because they break the CI-builds.

--- src/main/java/org/sonar/plugins/cxx/veraxx/CxxVeraxxRuleRepository.java
+++ src/main/java/org/sonar/plugins/cxx/veraxx/CxxVeraxxRuleRepository.java
@@ -1,6 +1,6 @@
 /*
  * Sonar Cxx Plugin, open source software quality management tool.
- * Copyright (C) 2010 - 2011, Neticoa SAS France - Tous droits réservés.
+ * Copyright (C) 2010 - 2011, Neticoa SAS France - Tous droits reserves.
  * Author(s) : Franck Bonin, Neticoa SAS France.
  *
  * Sonar Cxx Plugin is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * License along with Sonar Cxx Plugin; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-
 package org.sonar.plugins.cxx.veraxx;
 
 
