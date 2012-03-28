55e2c47477a249c7a35b2a4074699484ce10c1e8
Waleri Enns <waleri.enns@gmail.com>
2012-03-28 18:12:47 +0000

    Refactor the coverage sensor to use the abstract cobertura sensor which lives in the sonar cobertura plugin. This makes us depend on this plugin but it also dumps 100+ redundant lc.

--- src/main/java/org/sonar/plugins/cxx/gcovr/CxxGcovrSensor.java
+++ src/main/java/org/sonar/plugins/cxx/gcovr/CxxGcovrSensor.java
@@ -19,45 +19,23 @@
  */
 package org.sonar.plugins.cxx.gcovr;
 
-import static java.util.Locale.ENGLISH;
-import static org.sonar.api.utils.ParsingUtils.parseNumber;
-
 import java.io.File;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.stream.XMLStreamException;
 
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.lang.StringUtils;
-import org.codehaus.staxmate.in.SMHierarchicCursor;
-import org.codehaus.staxmate.in.SMInputCursor;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.sonar.api.batch.CoverageExtension;
 import org.sonar.api.batch.SensorContext;
-import org.sonar.api.measures.CoreMetrics;
-import org.sonar.api.measures.Measure;
-import org.sonar.api.measures.PersistenceMode;
-import org.sonar.api.measures.PropertiesBuilder;
 import org.sonar.api.resources.Project;
-import org.sonar.api.utils.StaxParser;
-import org.sonar.api.utils.XmlParserException;
+import org.sonar.api.resources.Resource;
+import org.sonar.plugins.cobertura.api.AbstractCoberturaParser;
 import org.sonar.plugins.cxx.CxxSensor;
 
-/**
- * TODO copied from sonar-cobertura-plugin with modifications: JavaFile replaced by C++, fixed SONARPLUGINS-696 C++ collectFileMeasures use
- * Project to locate C++ File getReports use FileSetManager for smarter report select using new now plugin configuration use Fileset (ex **
- * /coverage.xml)
- */
-
-public class CxxGcovrSensor extends CxxSensor {
+public class CxxGcovrSensor extends CxxSensor implements CoverageExtension{
   public static final String REPORT_PATH_KEY = "sonar.cxx.gcovr.reportPath";
   private static final String DEFAULT_REPORT_PATH = "gcovr-reports/gcovr-result-*.xml";
   private static Logger logger = LoggerFactory.getLogger(CxxGcovrSensor.class);
-
+  
   private Configuration conf = null;
 
   public CxxGcovrSensor(Configuration conf) {
@@ -67,162 +45,19 @@ public class CxxGcovrSensor extends CxxSensor {
   public void analyse(Project project, SensorContext context) {
     File[] reports = getReports(conf, project.getFileSystem().getBasedir().getPath(),
                                 REPORT_PATH_KEY, DEFAULT_REPORT_PATH);
-
-    Map<String, FileData> fileDataPerFilename = new HashMap<String, FileData>();
     for (File report : reports) {
-      parseReport(project, report, context, fileDataPerFilename);
-    }
-    for (FileData cci : fileDataPerFilename.values()) {
-      logger.debug("collectPackageMeasures fileKeyExist? {}", cci.getFile().getKey());
-      if (fileExist(context, cci.getFile())) {
-        logger.debug("collectPackageMeasures file Exist {}", cci.getFile().getKey());
-        for (Measure measure : cci.getMeasures()) {
-          logger.debug("collectPackageMeasures mesure value = {}", measure.toString());
-          context.saveMeasure(cci.getFile(), measure);
-        }
-      } else {
-        logger.warn("collectPackageMeasures file {} IS NOT inventoried ", cci.getFile().getKey());
-      }
+      parseReport(project, context, report);
     }
   }
 
-  private void parseReport(final Project project, File xmlFile, final SensorContext context, final Map<String, FileData> dataPerFilename) {
-    try {
-      logger.info("parsing gcovr report '{}'", xmlFile);
-      StaxParser parser = new StaxParser(new StaxParser.XmlStreamHandler() {
-
-        public void stream(SMHierarchicCursor rootCursor) throws XMLStreamException {
-          try {
-            rootCursor.advance();
-            collectPackageMeasures(project, rootCursor.descendantElementCursor("package"), context, dataPerFilename);
-          } catch (ParseException e) {
-            throw new XMLStreamException(e);
-          }
-        }
-      });
-      parser.parse(xmlFile);
-    } catch (XMLStreamException e) {
-      throw new XmlParserException(e);
-    }
-  }
-
-  private void collectPackageMeasures(Project project, SMInputCursor pack, SensorContext context, Map<String, FileData> dataPerFilename)
-      throws ParseException, XMLStreamException {
-    logger.debug("collectPackageMeasures");
-    while (pack.getNext() != null) {
-      collectFileMeasures(project, pack.descendantElementCursor("class"), dataPerFilename);
-    }
-  }
-
-  private boolean fileExist(SensorContext context, org.sonar.api.resources.File file) {
-    return context.getResource(file) != null;
-  }
-
-  private void collectFileMeasures(Project project, SMInputCursor clazz, Map<String, FileData> dataPerFilename) throws ParseException,
-      XMLStreamException {
-    logger.debug("collectFileMeasures");
-    while (clazz.getNext() != null) {
-      String fileName = clazz.getAttrValue("filename");
-
-      org.sonar.api.resources.File cxxfile =
-        org.sonar.api.resources.File.fromIOFile(new File(fileName), project);
-      if (cxxfile != null) {
-        String FileKey = cxxfile.getKey();
-        FileData data = dataPerFilename.get(FileKey);
-        if (data == null) {
-          data = new FileData(cxxfile);
-          dataPerFilename.put(FileKey, data);
-          logger.debug("collectFileMeasures created CXXFILe", data.getFile().getKey());
-        }
-        collectFileData(clazz, data);
+  protected void parseReport(final Project project, SensorContext context, File report) {
+    new AbstractCoberturaParser() {
+      @Override
+      protected Resource<?> getResource(String fileName) {
+        logger.debug("Creating resource for {}", fileName);
+        return org.sonar.api.resources.File.fromIOFile(new File(fileName), project);
       }
-      else{
-        //advance to next block without collecting
-        SMInputCursor line = clazz.childElementCursor("lines").advance().childElementCursor("line");
-        while (line.getNext() != null);
-      }
-    }
-  }
-
-  private void collectFileData(SMInputCursor clazz, FileData data) throws ParseException, XMLStreamException {
-
-    logger.debug("collectFileData");
-    SMInputCursor line = clazz.childElementCursor("lines").advance().childElementCursor("line");
-    while (line.getNext() != null) {
-      String lineId = line.getAttrValue("number");
-      data.addLine(lineId, (int) parseNumber(line.getAttrValue("hits"), ENGLISH));
-
-      String isBranch = line.getAttrValue("branch");
-      String text = line.getAttrValue("condition-coverage");
-      if (StringUtils.equals(isBranch, "true") && StringUtils.isNotBlank(text)) {
-        String[] conditions = StringUtils.split(StringUtils.substringBetween(text, "(", ")"), "/");
-        data.addConditionLine(lineId, Integer.parseInt(conditions[0]), Integer.parseInt(conditions[1]),
-            StringUtils.substringBefore(text, " "));
-      }
-    }
-  }
-
-  private class FileData {
-
-    private int lines = 0;
-    private int conditions = 0;
-    private int coveredLines = 0;
-    private int coveredConditions = 0;
-
-    private org.sonar.api.resources.File file;
-    private PropertiesBuilder<String, Integer> lineHitsBuilder = new PropertiesBuilder<String, Integer>(CoreMetrics.COVERAGE_LINE_HITS_DATA);
-    private PropertiesBuilder<String, String> branchHitsBuilder = new PropertiesBuilder<String, String>(
-        CoreMetrics.BRANCH_COVERAGE_HITS_DATA);
-
-    public void addLine(String lineId, int lineHits) {
-      lines++;
-      if (lineHits > 0) {
-        coveredLines++;
-      }
-      Map<String, Integer> props = lineHitsBuilder.getProps();
-      if (props.containsKey(lineId)) {
-        logger.debug("lineHitsBuilder find pre-existing line");
-        props.put(lineId, props.get(lineId) + lineHits);
-      } else {
-        lineHitsBuilder.add(lineId, lineHits);
-      }
-    }
-
-    public void addConditionLine(String lineId, int coveredConditions, int conditions, String label) {
-      this.conditions += conditions;
-      this.coveredConditions += coveredConditions;
-      Map<String, String> props = branchHitsBuilder.getProps();
-      if (props.containsKey(lineId)) {
-        logger.debug("branchHitsBuilder find pre-existing line");
-        props.put(lineId, props.get(lineId) + ", " + label);
-      } else {
-        branchHitsBuilder.add(lineId, label);
-      }
-    }
-
-    public FileData(org.sonar.api.resources.File file) {
-      this.file = file;
-    }
-
-    public List<Measure> getMeasures() {
-      List<Measure> measures = new ArrayList<Measure>();
-      if (lines > 0) {
-        measures.add(new Measure(CoreMetrics.LINES_TO_COVER, (double) lines));
-        measures.add(new Measure(CoreMetrics.UNCOVERED_LINES, (double) lines - coveredLines));
-        measures.add(lineHitsBuilder.build().setPersistenceMode(PersistenceMode.DATABASE));
-
-        if (conditions > 0) {
-          measures.add(new Measure(CoreMetrics.CONDITIONS_TO_COVER, (double) conditions));
-          measures.add(new Measure(CoreMetrics.UNCOVERED_CONDITIONS, (double) conditions - coveredConditions));
-          measures.add(branchHitsBuilder.build().setPersistenceMode(PersistenceMode.DATABASE));
-        }
-      }
-      return measures;
-    }
-
-    public org.sonar.api.resources.File getFile() {
-      return file;
-    }
+    }.parseReport(report, context);
   }
 
   @Override
