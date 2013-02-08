2c31457bb476419a7af980e423e74d8f1bdd2446
Jorge Costa <jmecosta@gmail.com>
2013-02-08 11:26:53 +0200

    Increse test count

--- src/test/java/org/sonar/plugins/cxx/externalrules/CxxExternalRulesSensorTest.java
+++ src/test/java/org/sonar/plugins/cxx/externalrules/CxxExternalRulesSensorTest.java
@@ -23,48 +23,70 @@ package org.sonar.plugins.cxx.externalrules;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.*;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.config.Settings;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.resources.Project;
 import org.sonar.api.resources.Resource;
-import org.sonar.api.rules.Rule;
 import org.sonar.api.rules.RuleFinder;
-import org.sonar.api.rules.RuleQuery;
 import org.sonar.api.rules.Violation;
+import org.sonar.api.utils.SonarException;
 import org.sonar.plugins.cxx.TestUtils;
 
 public class CxxExternalRulesSensorTest {
   private CxxExternalRulesSensor sensor;
   private SensorContext context;
   private Project project;
-
-  public static RuleFinder mockRuleFinder(){
-    Rule ruleMock = Rule.create("cxxexternal", "cxxexternal-unusedFunction", "unusedFunction");
-    RuleFinder ruleFinder = mock(RuleFinder.class);
-    when(ruleFinder.findByKey((String) anyObject(),
-        (String) anyObject())).thenReturn(ruleMock);
-    when(ruleFinder.find((RuleQuery) anyObject())).thenReturn(ruleMock);
-    return ruleFinder;
-  }
+  private RuleFinder ruleFinder;
+  private RulesProfile profile;
+  private Settings settings;
   
   @Before
   public void setUp() {
     project = TestUtils.mockProject();
-    RuleFinder ruleFinder = TestUtils.mockRuleFinder();
-    RulesProfile profile = mock(RulesProfile.class);
-    sensor = new CxxExternalRulesSensor(ruleFinder, new Settings(), profile);
+    ruleFinder = TestUtils.mockRuleFinder();
+    profile = mock(RulesProfile.class);    
     context = mock(SensorContext.class);
+    settings = new Settings();
     Resource resourceMock = mock(Resource.class);
     when(context.getResource((Resource)anyObject())).thenReturn(resourceMock);
   }
 
   @Test
-  public void shouldReportCorrectViolations() {
+  public void shouldReportCorrectViolations() {    
+    settings.setProperty(CxxExternalRulesSensor.REPORT_PATH_KEY,
+            "externalrules-reports/externalrules-result-ok.xml");    
+    sensor = new CxxExternalRulesSensor(ruleFinder, settings, profile);
     sensor.analyse(project, context);
     verify(context, times(2)).saveViolation(any(Violation.class));
   }
+  
+  @Test(expected=SonarException.class)
+  public void shouldThrowExceptionWhenReportEmpty() {    
+    settings.setProperty(CxxExternalRulesSensor.REPORT_PATH_KEY,
+            "externalrules-reports/externalrules-result-empty.xml");    
+    sensor = new CxxExternalRulesSensor(ruleFinder, settings, profile);
+    sensor.analyse(project, context);
+    verify(context, times(0)).saveViolation(any(Violation.class));
+  }
+  
+  @Test
+  public void shouldReportNoViolationsNoReportFound() {
+    settings = new Settings();
+    settings.setProperty(CxxExternalRulesSensor.REPORT_PATH_KEY,
+            "externalrules-reports/noreport.xml");    
+    sensor = new CxxExternalRulesSensor(ruleFinder, settings, profile);
+    sensor.analyse(project, context);
+    verify(context, times(0)).saveViolation(any(Violation.class));
+  }  
+  
+  @Test(expected=SonarException.class)
+  public void shouldReportNoViolationsInvalidReport() {
+    settings = new Settings();
+    settings.setProperty(CxxExternalRulesSensor.REPORT_PATH_KEY,
+            "externalrules-reports/externalrules-result-invalid.xml");
+    sensor = new CxxExternalRulesSensor(ruleFinder, settings, profile);
+    sensor.analyse(project, context);
+  }  
 }
