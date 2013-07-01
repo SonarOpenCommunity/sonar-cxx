2613dd46e72ac59032919706423cf94391288762
Waleri Enns <wen@contact.de>
2013-07-01 21:19:27 +0200

    SONARPLUGINS-2804: Add rules for CppCheck V1.58, V1.59 and V1.60 Initial patch authored by Bert Kandert.

--- sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/cppcheck/CxxCppCheckRuleRepositoryTest.java
+++ sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/cppcheck/CxxCppCheckRuleRepositoryTest.java
@@ -31,6 +31,6 @@ public class CxxCppCheckRuleRepositoryTest {
   public void createRulesTest() {
     CxxCppCheckRuleRepository rulerep = new CxxCppCheckRuleRepository(
         mock(ServerFileSystem.class), new XMLRuleParser());
-    assertThat(rulerep.createRules()).hasSize(274);
+    assertThat(rulerep.createRules()).hasSize(288);
   }
 }
