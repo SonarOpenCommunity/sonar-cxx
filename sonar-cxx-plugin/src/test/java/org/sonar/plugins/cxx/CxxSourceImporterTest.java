4d5fd8e47740c1aa141402be79919661bb1a2dae
jmecosta <jmecosta@gmail.com>
2014-03-17 09:47:29 +0100

    Remove end lines for test file so it works always

--- sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/CxxSourceImporterTest.java
+++ sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/CxxSourceImporterTest.java
@@ -49,7 +49,7 @@ public class CxxSourceImporterTest {
 
     importer.analyse(project, context);
 
-    verify(context).saveSource((Resource<?>) anyObject(), eq(String.format("<c++ source>%n")));
+    verify(context).saveSource((Resource<?>) anyObject(), eq("<c++ source>"));
   }
 
   private Project mockProject() {
