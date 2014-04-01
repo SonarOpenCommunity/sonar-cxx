b5c315a53ae7c40ade58338d8cc24404be6cf51f
guwirth <guenter.wirth@etas.com>
2014-04-01 16:53:12 +0200

    handle error recovery on global declaration level: skip syntax errors and proceed with next declaration add switch sonar.cxx.errorRecoveryEnabled=true/false add syntax error logging add a rule to report on every recoveredDeclaration (C++ skip parser error)

---
+++ cxx-squid/src/test/resources/parser/own/error_recovery_declaration.cc
@@ -0,0 +1,19 @@
+int function_1()
+{
+    return 1;
+}
+
+int function_2()
+{
+    return 2//; <= syntax error
+}
+
+int function_3()
+{
+    return 3;
+}
+
+int function_4()
+{
+    return 4;
+//} <= syntax error
