9028d733153c2737ec740c37083bc711ce8d4c91
Jorge Costa <jorge.costa@tekla.com>
2014-04-19 14:14:26 +0300

    #171: Fix compatibility with sonar 3.7

--- sonar-cxx-plugin/src/main/java/org/sonar/plugins/cxx/CxxRuleRepository.java
+++ sonar-cxx-plugin/src/main/java/org/sonar/plugins/cxx/CxxRuleRepository.java
@@ -25,7 +25,55 @@ import org.sonar.api.rules.RuleRepository;
 import org.sonar.cxx.checks.CheckList;
 
 import java.util.List;
+import org.sonar.api.Properties;
+import org.sonar.api.Property;
+import org.sonar.api.PropertyType;
+import org.sonar.plugins.cxx.compiler.CxxCompilerGccRuleRepository;
+import org.sonar.plugins.cxx.compiler.CxxCompilerVcRuleRepository;
+import org.sonar.plugins.cxx.cppcheck.CxxCppCheckRuleRepository;
+import org.sonar.plugins.cxx.externalrules.CxxExternalRuleRepository;
+import org.sonar.plugins.cxx.pclint.CxxPCLintRuleRepository;
+import org.sonar.plugins.cxx.rats.CxxRatsRuleRepository;
+import org.sonar.plugins.cxx.valgrind.CxxValgrindRuleRepository;
+import org.sonar.plugins.cxx.veraxx.CxxVeraxxRuleRepository;
 
+/**
+ * Creates FXCop rule repositories for every language supported by FxCop.
+ */
+@Properties({
+  @Property(key = CxxCompilerVcRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "Compiler custom rules for Visual C++",
+    description = "XML description of Compiler custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxCompilerGccRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "Compiler custom rules for GCC",
+    description = "XML description of Compiler custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxCppCheckRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "CppCheck custom rules",
+    description = "XML description of CppCheck custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxPCLintRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "PCLint custom rules",
+    description = "XML description of PCLint custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxRatsRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "Rats custom rules",
+    description = "XML description of Rats custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxValgrindRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "Valgrind custom rules",
+    description = "XML description of Valgrind custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxVeraxxRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "Vera++ custom rules",
+    description = "XML description of Vera++ custom rules", type = PropertyType.TEXT,
+    global = true, project = false),
+  @Property(key = CxxExternalRuleRepository.CUSTOM_RULES_KEY,
+    defaultValue = "", name = "External custom rules",
+    description = "XML description of External custom rules", type = PropertyType.TEXT,
+    global = true, project = false)      
+})
 public class CxxRuleRepository extends RuleRepository {
 
   private static final String REPOSITORY_NAME = "Sonar";
