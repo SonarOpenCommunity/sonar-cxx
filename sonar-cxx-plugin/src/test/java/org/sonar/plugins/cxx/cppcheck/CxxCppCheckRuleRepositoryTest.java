b299b758bc89c8e8e9a2475522f96e06b5b91cc6
jmecosta <jmecosta@gmail.com>
2014-04-25 09:04:18 +0200

    Support for cppcheck 1.63

--- sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/cppcheck/CxxCppCheckRuleRepositoryTest.java
+++ sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/cppcheck/CxxCppCheckRuleRepositoryTest.java
@@ -32,6 +32,6 @@ public class CxxCppCheckRuleRepositoryTest {
   public void createRulesTest() {
     CxxCppCheckRuleRepository rulerep = new CxxCppCheckRuleRepository(
         mock(ServerFileSystem.class), new XMLRuleParser(), new Settings());
-    assertEquals(rulerep.createRules().size(), 289);
+    assertEquals(295, rulerep.createRules().size());
   }
 }
