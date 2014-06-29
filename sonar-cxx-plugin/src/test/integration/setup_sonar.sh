3602ab804b97d44f45c9074dad2d4da4dd395fcb
Waleri Enns <waleri.enns@gmail.com>
2014-06-30 00:06:51 +0200

    Prepare next development iteration; extend CI checks by the smoketests

---
+++ sonar-cxx-plugin/src/test/integration/setup_sonar.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget http://dist.sonar.codehaus.org/sonarqube-4.2.zip
+unzip sonarqube-4.2.zip
+wget http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/2.4/sonar-runner-dist-2.4.zip
+unzip sonar-runner-dist-2.4.zip
