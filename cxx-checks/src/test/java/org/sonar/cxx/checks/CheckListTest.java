f30f0e152cc5f0f9b9a1c3cfba1d5ddfe5f098e4
guwirth <guenter.wirth@etas.com>
2014-12-28 19:40:30 +0100

    Line RegEx rule This rule template can be used to create rules which will be triggered when a line matches a given regular expression. For each match a line violation will be created. Additional a Ant-style matching pattern for the path can be defined.

--- cxx-checks/src/test/java/org/sonar/cxx/checks/CheckListTest.java
+++ cxx-checks/src/test/java/org/sonar/cxx/checks/CheckListTest.java
@@ -27,6 +27,6 @@ public class CheckListTest {
 
   @Test
   public void count() {
-    assertThat(CheckList.getChecks().size()).isEqualTo(37);
+    assertThat(CheckList.getChecks().size()).isEqualTo(38);
   }
 }
