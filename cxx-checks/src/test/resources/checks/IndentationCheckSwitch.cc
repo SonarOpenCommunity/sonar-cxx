001dba8e2dabfd9549ed5f6bbd884d25eb1cd1b6
jmecosta <jmecosta@gmail.com>
2015-01-20 22:13:13 +0200

    create test case to demonstrate wrong identation

--- cxx-checks/src/test/resources/checks/IndentationCheckSwitch.cc
+++ cxx-checks/src/test/resources/checks/IndentationCheckSwitch.cc
@@ -32,8 +32,8 @@ int FooY() {
 
     switch(x)
     {
-    case 0: case 1: case 2: case 3:
-        break;
+    case 0: case 1: case 2: case 3: // can this made configurable?
+        break; // says needs to be in 33 column
     default: break;
     }
 };
