ec7bed0e40f4e4600565e15bf05cf110a4c52678
Arnold Metselaar <arnold.metselaar@cgi.com>
2015-01-22 10:09:38 +0100

    Uncomment another @Override.

--- cxx-checks/src/main/java/org/sonar/cxx/checks/ReservedNamesCheck.java
+++ cxx-checks/src/main/java/org/sonar/cxx/checks/ReservedNamesCheck.java
@@ -83,7 +83,7 @@ public class ReservedNamesCheck extends SquidCheck<Grammar> implements CxxCharse
     }
   }
 
-  //@Override
+  @Override
   public void setCharset(Charset charset) {
     this.charset = charset;
   }
