38e726b3d5e9c76a3a4c2d06e0b92ad7461464cc
Waleri Enns <waleri.enns@gmail.com>
2015-01-28 23:39:32 +0100

    BDD-testsuite: Update to gtest-1.7 and use the RecordProperty()-Way of augmenting the XML reports

--- integration-tests/testdata/googletest_project/tests/gtest-1.7.0/gtest/gtest_main.cc
+++ integration-tests/testdata/googletest_project/tests/gtest-1.7.0/gtest/gtest_main.cc
@@ -1,4 +1,4 @@
-// Copyright 2008, Google Inc.
+// Copyright 2006, Google Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -26,23 +26,13 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Author: mheule@google.com (Markus Heule)
-//
-// Google C++ Testing Framework (Google Test)
-//
-// Sometimes it's desirable to build Google Test by compiling a single file.
-// This file serves this purpose.
 
-// This line ensures that gtest.h can be compiled on its own, even
-// when it's fused.
+#include <stdio.h>
+
 #include "gtest/gtest.h"
 
-// The following lines pull in the real gtest *.cc files.
-#include "src/gtest.cc"
-#include "src/gtest-death-test.cc"
-#include "src/gtest-filepath.cc"
-#include "src/gtest-port.cc"
-#include "src/gtest-printers.cc"
-#include "src/gtest-test-part.cc"
-#include "src/gtest-typed-test.cc"
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
