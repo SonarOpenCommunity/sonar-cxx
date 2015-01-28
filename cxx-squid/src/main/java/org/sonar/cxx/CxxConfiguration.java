42884c7943a42fc635f1e2395197ea22543c8cf5
Jorge Costa <Jorge.Costa@tekla.com>
2015-01-28 20:14:12 +0200

    if bullseye is installed it will hijack the cl.exe command

--- cxx-squid/src/main/java/org/sonar/cxx/CxxConfiguration.java
+++ cxx-squid/src/main/java/org/sonar/cxx/CxxConfiguration.java
@@ -196,6 +196,11 @@ public class CxxConfiguration extends SquidConfiguration {
           if (line.startsWith("Target \"ClCompile\" in file")) {
             currentProjectPath = line.split("\" from project \"")[1].split("\\s+")[0].replace("\"", "");              
           }
+          
+          if (line.contains("C:\\Program Files (x86)\\BullseyeCoverage\\bin\\CL.exe")) {
+            parseVCppCompilerCLLine(line, currentProjectPath);
+          }
+          
           if (line.contains("C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\bin\\CL.exe") || 
                   line.contains("C:\\Program Files\\Microsoft Visual Studio 10.0\\VC\\bin\\CL.exe")) {
             parseVCppCompilerCLLine(line, currentProjectPath);
