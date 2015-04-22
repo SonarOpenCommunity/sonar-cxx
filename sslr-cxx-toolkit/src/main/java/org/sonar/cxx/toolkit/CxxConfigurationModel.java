efac880a6bdd8f75779f2aa13b5856a95e547335
guwirth <guenter.wirth@etas.com>
2015-04-22 18:19:50 +0200

    align sslr toolkit with phyton and javascript.
    toolkit is starting again now
    but still not able to parse
    add missing packages to fix 'NoClassDefFoundError: org/sonar/api/batch/fs/InputPath'

---
+++ sslr-cxx-toolkit/src/main/java/org/sonar/cxx/toolkit/CxxConfigurationModel.java
@@ -0,0 +1,105 @@
+/*
+ * Sonar C++ Plugin (Community)
+ * Copyright (C) 2011 Waleri Enns and CONTACT Software GmbH
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.cxx.toolkit;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.sonar.sslr.api.Grammar;
+import com.sonar.sslr.impl.Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.colorizer.CDocTokenizer;
+import org.sonar.colorizer.CppDocTokenizer;
+import org.sonar.colorizer.JavadocTokenizer;
+import org.sonar.colorizer.KeywordsTokenizer;
+import org.sonar.colorizer.StringTokenizer;
+import org.sonar.colorizer.Tokenizer;
+import org.sonar.sslr.toolkit.AbstractConfigurationModel;
+import org.sonar.sslr.toolkit.ConfigurationProperty;
+import org.sonar.sslr.toolkit.Validators;
+import org.sonar.cxx.CxxConfiguration;
+import org.sonar.cxx.api.CxxKeyword;
+import org.sonar.cxx.parser.CxxParser;
+import org.sonar.squidbridge.SquidAstVisitorContext;
+import org.sonar.squidbridge.SquidAstVisitorContextImpl;
+import org.sonar.squidbridge.api.SourceProject;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class CxxConfigurationModel extends AbstractConfigurationModel {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CxxConfigurationModel.class);
+
+  private static final String CHARSET_PROPERTY_KEY = "sonar.sourceEncoding";
+
+  @VisibleForTesting
+  ConfigurationProperty charsetProperty = new ConfigurationProperty("Charset", CHARSET_PROPERTY_KEY,
+    getPropertyOrDefaultValue(CHARSET_PROPERTY_KEY, "UTF-8"),
+    Validators.charsetValidator());
+
+  @Override
+  public List<ConfigurationProperty> getProperties() {
+    return ImmutableList.of(charsetProperty);
+  }
+
+  @Override
+  public Charset getCharset() {
+    return Charset.forName(charsetProperty.getValue());
+  }
+
+  @Override
+  public Parser<? extends Grammar> doGetParser() {
+    SquidAstVisitorContext<Grammar> context
+      = new SquidAstVisitorContextImpl<>(new SourceProject(""));
+    return CxxParser.create(context, getConfiguration());
+  }
+
+  @Override
+  public List<Tokenizer> doGetTokenizers() {
+    return ImmutableList.of(
+      new StringTokenizer("<span class=\"s\">", "</span>"),
+      new CDocTokenizer("<span class=\"cd\">", "</span>"),
+      new JavadocTokenizer("<span class=\"cppd\">", "</span>"),
+      new CppDocTokenizer("<span class=\"cppd\">", "</span>"),
+      new KeywordsTokenizer("<span class=\"k\">", "</span>", CxxKeyword.keywordValues()));
+  }
+
+  @VisibleForTesting
+  CxxConfiguration getConfiguration() {
+    CxxConfiguration config = new CxxConfiguration(getCharset());
+    config.setErrorRecoveryEnabled(false);
+    return config;
+  }
+
+  @VisibleForTesting
+  static String getPropertyOrDefaultValue(String propertyKey, String defaultValue) {
+    String propertyValue = System.getProperty(propertyKey);
+
+    if (propertyValue == null) {
+      LOG.info("The property \"" + propertyKey + "\" is not set, using the default value \"" + defaultValue + "\".");
+      return defaultValue;
+    } else {
+      LOG.info("The property \"" + propertyKey + "\" is set, using its value \"" + propertyValue + "\".");
+      return propertyValue;
+    }
+  }
+
+}
