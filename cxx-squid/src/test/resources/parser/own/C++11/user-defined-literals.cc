5f898b8b8bdc2526bc9cd98f2386a98c7c217d07
guwirth <guenter.wirth@etas.com>
2015-07-24 11:20:07 +0200

    user defined literals
    add user defined literal support (have to start with _)
    close #564
    standard user defined literals is the same (without _ at the beginning)
    close #570

--- cxx-squid/src/test/resources/parser/own/C++11/user-defined-literals.cc
+++ cxx-squid/src/test/resources/parser/own/C++11/user-defined-literals.cc
@@ -23,11 +23,21 @@ void operator"" _print(const char* str)
   std::cout << str;
 }
 
-//@todo - make this work
-int main() {
-  //double x = 90.0_deg;
+int example1() {
+  double x = 90.0_deg;
   std::cout << std::fixed << x << '\n';
-  //mytype y = 123_mytype;
+  mytype y = 123_mytype;
   std::cout << y.m << '\n';
-  //0x123ABC_print;
+  0x123ABC_print;
+}
+
+long double operator "" _w(long double);
+std::string operator "" _w(const char16_t*, std::size_t);
+unsigned operator "" _w(const char*);
+
+int example2() {
+  1.2_w;    // calls operator "" _w(1.2L)
+  u"one"_w; // calls operator "" _w(u"one", 3)
+  12_w;     // calls operator "" _w("12")
+  "two"_w;  // error: no applicable literal operator
 }
