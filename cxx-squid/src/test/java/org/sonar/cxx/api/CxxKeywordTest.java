1f5dedea215a02c9512235aacebde0f9fddcf989
guwirth <guenter.wirth@etas.com>
2015-11-08 12:31:53 +0100

    remove final and override from keyword list
    final and override have a special meaning: http://en.cppreference.com/w/cpp/keyword and are no keywords
    close #673

--- cxx-squid/src/test/java/org/sonar/cxx/api/CxxKeywordTest.java
+++ cxx-squid/src/test/java/org/sonar/cxx/api/CxxKeywordTest.java
@@ -27,7 +27,7 @@ public class CxxKeywordTest {
 
   @Test
   public void test() {
-    assertThat(CxxKeyword.values()).hasSize(86);
+    assertThat(CxxKeyword.values()).hasSize(84);
     assertThat(CxxKeyword.keywordValues()).hasSize(CxxKeyword.values().length);
   }
 
