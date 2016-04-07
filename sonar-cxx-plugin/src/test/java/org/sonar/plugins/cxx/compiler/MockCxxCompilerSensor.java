21eeb93d601e207765534da1d7c8fdcc9c195165
Andre <Andre>
2016-04-07 16:00:13 +0200

    Add missing code header and minor code cleanups

--- sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/compiler/MockCxxCompilerSensor.java
+++ sonar-cxx-plugin/src/test/java/org/sonar/plugins/cxx/compiler/MockCxxCompilerSensor.java
@@ -1,3 +1,22 @@
+/*
+ * Sonar C++ Plugin (Community)
+ * Copyright (C) 2010 Neticoa SAS France
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.plugins.cxx.compiler;
 
 import static org.mockito.Matchers.any;
@@ -18,7 +37,6 @@ import org.sonar.api.component.ResourcePerspectives;
 import org.sonar.api.config.Settings;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.resources.Project;
-import org.sonar.plugins.cxx.compiler.CompilerParser.Warning;
 
 public class MockCxxCompilerSensor extends CxxCompilerSensor {
 
@@ -58,15 +76,8 @@ public class MockCxxCompilerSensor extends CxxCompilerSensor {
   }
 
   @Override
-  public String getParserStringProperty(String name, String def) {
-    // TODO Auto-generated method stub
-    return super.getParserStringProperty(name, def);
-  }
-
-  @Override
   public void saveUniqueViolation(Project project, SensorContext context, String ruleRepoKey, String file,
       String line, String ruleId, String msg) {
-    //       super.saveUniqueViolation(project, context, ruleRepoKey, file, line, ruleId, msg);
     
     CompilerParser.Warning w = new CompilerParser.Warning(file, line, ruleId, msg);
     savedWarnings.add(w);
