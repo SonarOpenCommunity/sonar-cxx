8cb0bed3cb82a67616a4fe7613bd6870715a2c1b
guwirth <guenter.wirth@etas.com>
2017-11-05 21:18:48 +0100

    add two missing cppcheck rules: - cppcheck:gmtimeCalled & cppcheck:ftimeCalled - close #1280

--- cxx-sensors/src/test/java/org/sonar/cxx/sensors/cppcheck/CxxCppCheckRuleRepositoryTest.java
+++ cxx-sensors/src/test/java/org/sonar/cxx/sensors/cppcheck/CxxCppCheckRuleRepositoryTest.java
@@ -43,6 +43,6 @@ public class CxxCppCheckRuleRepositoryTest {
     def.define(context);
 
     RulesDefinition.Repository repo = context.repository(CxxCppCheckRuleRepository.KEY);
-    assertEquals(374, repo.rules().size());
+    assertEquals(376, repo.rules().size());
   }
 }
