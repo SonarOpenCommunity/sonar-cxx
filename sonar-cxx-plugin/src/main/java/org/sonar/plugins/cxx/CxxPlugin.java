51b7dda6ae7bd68812a17573be3577e20853bb2c
guwirth <guenter.wirth@etas.com>
2019-01-09 17:21:44 +0100

    Collections.unmodifiableList

--- sonar-cxx-plugin/src/main/java/org/sonar/plugins/cxx/CxxPlugin.java
+++ sonar-cxx-plugin/src/main/java/org/sonar/plugins/cxx/CxxPlugin.java
@@ -96,7 +96,7 @@ public final class CxxPlugin implements Plugin {
 
   private static List<PropertyDefinition> generalProperties() {
     String subcateg = "(1) General";
-    return new ArrayList<>(Arrays.asList(
+    return Collections.unmodifiableList(Arrays.asList(
       PropertyDefinition.builder(SOURCE_FILE_SUFFIXES_KEY)
         .multiValues(true)
         .defaultValue(CppLanguage.DEFAULT_SOURCE_SUFFIXES)
@@ -197,7 +197,7 @@ public final class CxxPlugin implements Plugin {
 
   private static List<PropertyDefinition> codeAnalysisProperties() {
     String subcateg = "(2) Code analysis";
-    return new ArrayList<>(Arrays.asList(PropertyDefinition.builder(LANG_PROP_PREFIX
+    return Collections.unmodifiableList(Arrays.asList(PropertyDefinition.builder(LANG_PROP_PREFIX
       + CxxCppCheckSensor.REPORT_PATH_KEY)
       .name("Cppcheck report(s)")
       .description("Path to a <a href='http://cppcheck.sourceforge.net/'>Cppcheck</a> analysis XML report, relative to"
@@ -378,7 +378,7 @@ public final class CxxPlugin implements Plugin {
 
   private static List<PropertyDefinition> compilerWarningsProperties() {
     String subcateg = "(4) Compiler warnings";
-    return new ArrayList<>(Arrays.asList(
+    return Collections.unmodifiableList(Arrays.asList(
       PropertyDefinition.builder(LANG_PROP_PREFIX + CxxCompilerVcSensor.REPORT_PATH_KEY)
         .name("VC Compiler Report(s)")
         .description("Path to compilers output (i.e. file(s) containg compiler warnings), relative to projects root."
@@ -454,7 +454,7 @@ public final class CxxPlugin implements Plugin {
 
   private static List<PropertyDefinition> testingAndCoverageProperties() {
     String subcateg = "(3) Testing & Coverage";
-    return new ArrayList<>(Arrays.asList(
+    return Collections.unmodifiableList(Arrays.asList(
       PropertyDefinition.builder(LANG_PROP_PREFIX + CxxCoverageSensor.REPORT_PATH_KEY)
         .name("Unit test coverage report(s)")
         .description("List of paths to reports containing unit test coverage data, relative to projects root."
@@ -517,7 +517,7 @@ public final class CxxPlugin implements Plugin {
 
   private static List<PropertyDefinition> duplicationsProperties() {
     String subcateg = "(5) Duplications";
-    return new ArrayList<>(Arrays.asList(
+    return Collections.unmodifiableList(Arrays.asList(
       PropertyDefinition.builder(CxxPlugin.CPD_IGNORE_LITERALS_KEY)
         .defaultValue(Boolean.FALSE.toString())
         .name("Ignores literal value differences when evaluating a duplicate block")
